fa2018a2f567b67c36e5dd8a5519ebaf20ad9c61
Deadwoodxbruh <aakash.gudivada@gmail.com>
2023-09-10 22:28:03 +0530

    k
    k

--- mainpage.js
+++ mainpage.js
@@ -1,11 +1,18 @@
 const lettercontent = document.getElementsByClassName("lettercontent");
+const contactinfo = document.getElementsByClassName("contactinfo");
 const contentinfohome = document.getElementsByClassName("contentinfohome");
 const publicimg = document.getElementsByClassName("publicimg");
 
-const ctteclass = document.getElementsByClassName("cttecontent");
+const ctteclass = document.getElementsByClassName("coml");
+const ltitle = document.getElementById("ltitle");
+const atitle = document.getElementById("atitle");
 
 async function hideunhidethecontent(hideyesorno){
     if (hideyesorno === true){
+        ltitle.style.display = "none";
+        atitle.style.display = "none";
+        document.getElementById("ctitle").style.display = "flex";
+        document.getElementById("cdesc").style.display = "flex";
         for (var i = 0; i < lettercontent.length; i++) {
             lettercontent[i].style.display = "none";
         }
@@ -15,7 +22,11 @@ async function hideunhidethecontent(hideyesorno){
         for (var i = 0; i < publicimg.length; i++) {
             publicimg[i].style.display = "none";
         }
-    }else{
+    }else if (hideyesorno === false){
+        ltitle.style.display = "flex";
+        atitle.style.display = "flex";
+        document.getElementById("ctitle").style.display = "none";
+        document.getElementById("cdesc").style.display = "none";
         for (var i = 0; i < lettercontent.length; i++) {
             lettercontent[i].style.display = "flex";
         }
@@ -28,11 +39,71 @@ async function hideunhidethecontent(hideyesorno){
     }
 }
 
-document.getElementById("ctte").addEventListener("click",function(){
-    hideunhidethecontent(true);
-    for (var i = 0; i < ctteclass.length; i++) {
-        ctteclass[i].style.display = "block";
+async function opencommite(variftrue){
+    if (variftrue === true){
+        hideunhidethecontent(true);
+        for (var i = 0; i < ctteclass.length; i++) {
+            ctteclass[i].style.display = "flex";
+        }
+    }else if (variftrue === false){
+        hideunhidethecontent(false);
+        for (var i = 0; i < ctteclass.length; i++) {
+            ctteclass[i].style.display = "none";
+        }
+    }else{
+        for (var i = 0; i < ctteclass.length; i++) {
+            ctteclass[i].style.display = "none";
+        } 
     }
+}
+
+async function opencontact(opencontactvar){
+    if (opencontactvar === true){
+        document.getElementById("ctitle").style.display = "none";
+        document.getElementById("cdesc").style.display = "none";
+        for (var i = 0; i < contactinfo.length; i++) {
+            contactinfo[i].style.display = "flex";
+        }
+    }else{
+        for (var i = 0; i < contactinfo.length; i++) {
+            contactinfo[i].style.display = "none";
+        }
+    }
+}
+
+const burgerclass = document.querySelector(".burgerframe");
+
+document.getElementById("ctte").addEventListener("click",function(){
+    opencommite(true);
+    opencontact(false)
+})
+
+document.getElementById("home").addEventListener("click",function(){
+    opencommite(false);
+    opencontact(false);
+})
+
+document.getElementById("mobilecb").addEventListener("click",function(){
+    hideunhidethecontent(true);
+    opencontact(true);
+    opencommite();
+    burgerclass.style.display = "none"
+})
+
+document.getElementById("mobhb").addEventListener("click",function(){
+    opencommite(false);
+    burgerclass.style.display = "none"
+})
+
+document.getElementById("contactbn").addEventListener("click",function(){
+    hideunhidethecontent(true);
+    opencontact(true);
+    opencommite()
+})
+
+document.getElementById("mobcb").addEventListener("click",function(){
+    opencontact(true);
+    burgerclass.style.display = "none"
 })
 
 document.getElementById("home").addEventListener("click",function(){
@@ -43,10 +114,13 @@ document.getElementById("home").addEventListener("click",function(){
 })
 
 document.getElementById("burgericon").addEventListener("click",function(){
-    const burgerclass = document.querySelector(".burgerframe");
     if (burgerclass.style.display === "flex"){
         burgerclass.style.display = "none"
     }else{
         burgerclass.style.display = "flex"
     }
+})
+
+document.getElementById("logo").addEventListener("click",function(){
+    window.location.href = "https://meluhamun2023.github.io"
 })
