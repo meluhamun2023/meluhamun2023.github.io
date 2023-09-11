f791ccf9aa0784c3a5c6cd4d8a82d9037e552ad8
Deadwoodxbruh <aakash.gudivada@gmail.com>
2023-09-11 23:39:25 +0530

    ok
    ok

--- mainpage.js
+++ mainpage.js
@@ -1,43 +1,22 @@
-const lettercontent = document.getElementsByClassName("lettercontent");
-const contactinfo = document.getElementsByClassName("contactinfo");
-const contentinfohome = document.getElementsByClassName("contentinfohome");
+const lettercontent = document.getElementsByClassName("contentpage");
 const publicimg = document.getElementsByClassName("publicimg");
-
-const ctteclass = document.getElementsByClassName("coml");
-const ltitle = document.getElementById("ltitle");
-const atitle = document.getElementById("atitle");
 
 async function hideunhidethecontent(hideyesorno){
     if (hideyesorno === true){
-        ltitle.style.display = "none";
-        atitle.style.display = "none";
-        document.getElementById("ctitle").style.display = "flex";
-        document.getElementById("cdesc").style.display = "flex";
+        ltitle.style.displ
         for (var i = 0; i < lettercontent.length; i++) {
             lettercontent[i].style.display = "none";
         }
-        for (var i = 0; i < contentinfohome.length; i++) {
-            contentinfohome[i].style.display = "none";
-        }
-        for (var i = 0; i < publicimg.length; i++) {
-            publicimg[i].style.display = "none";
-        }
     }else if (hideyesorno === false){
-        ltitle.style.display = "flex";
-        atitle.style.display = "flex";
-        document.getElementById("ctitle").style.display = "none";
-        document.getElementById("cdesc").style.display = "none";
         for (var i = 0; i < lettercontent.length; i++) {
             lettercontent[i].style.display = "flex";
         }
-        for (var i = 0; i < contentinfohome.length; i++) {
-            contentinfohome[i].style.display = "flex";
-        }
-        for (var i = 0; i < publicimg.length; i++) {
-            publicimg[i].style.display = "flex";
-        }
     }
 }
+
+document.getElementById("sec").addEventListener("click",function(){
+    hideunhidethecontent(true)
+})
 
 async function opencommite(variftrue){
     if (variftrue === true){
@@ -76,11 +55,6 @@ const burgerclass = document.querySelector(".burgerframe");
 document.getElementById("ctte").addEventListener("click",function(){
     opencommite(true);
     opencontact(false)
-})
-
-document.getElementById("home").addEventListener("click",function(){
-    opencommite(false);
-    opencontact(false);
 })
 
 document.getElementById("mobilecb").addEventListener("click",function(){
